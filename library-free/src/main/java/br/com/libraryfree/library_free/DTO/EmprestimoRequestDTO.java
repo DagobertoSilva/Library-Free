47df66da153f9158a8436b9dde2baf0c2114acb3
Joao <142902287+JoaoMNZ@users.noreply.github.com>
2025-07-09 09:17:52 -0300

    Delete library-free directory

--- library-free/src/main/java/br/com/libraryfree/library_free/DTO/EmprestimoRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package br.com.libraryfree.library_free.DTO;
-
-import java.time.LocalDate;
-
-public record EmprestimoRequestDTO(Long alunoId, Long livroId, LocalDate prazoDevolucao) {
-}
--- library-free/src/main/java/br/com/libraryfree/library_free/DTO/LoginDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.libraryfree.library_free.DTO;
-
-public record LoginDTO(String email, String senha){
-}
